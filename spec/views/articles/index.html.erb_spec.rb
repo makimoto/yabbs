8d21ee93af855fca74184fd52008b2b3e4f5e295
Shimpei Makimoto <makimoto@tsuyabu.in>
2013-01-27 11:18:10 +0900

    Fix test example for articles#index

--- spec/views/articles/index.html.erb_spec.rb
+++ spec/views/articles/index.html.erb_spec.rb
@@ -6,12 +6,14 @@ describe "articles/index" do
       stub_model(Article,
         :title => "Title",
         :name => "Name",
-        :body => "MyText"
+        :body => "MyText",
+        :created_at => Time.now,
       ),
       stub_model(Article,
         :title => "Title",
         :name => "Name",
-        :body => "MyText"
+        :body => "MyText",
+        :created_at => Time.now,
       )
     ])
   end
@@ -19,8 +21,8 @@ describe "articles/index" do
   it "renders a list of articles" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Title".to_s, :count => 2
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    assert_select ".article>h2", :text => "Title".to_s, :count => 2
+    assert_select ".article>.article_meta", :text => /Name/, :count => 2
+    assert_select ".article>.article_body", :text => "MyText", :count => 2
   end
 end
