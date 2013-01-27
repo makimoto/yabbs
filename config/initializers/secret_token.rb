e0a07fcd0ecc5204d1c9033df7351937e3bf961b
Shimpei Makimoto <makimoto@tsuyabu.in>
2013-01-27 10:22:22 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yabbs::Application.config.secret_token = '2734fe7630eca4da5113ed6021b8592ba42bffc4dd25555beef388216de9df58f4366e2d93ad6c204368f7a2f85918891e467aea4d4ca10de23181b98251b694'
