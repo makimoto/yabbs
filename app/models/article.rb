4385152cddee600e4ae0a6113902f80e93c1ee86
Shimpei Makimoto <makimoto@tsuyabu.in>
2013-02-15 23:35:31 +0900

    Capistrano

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,11 @@
 class Article < ActiveRecord::Base
   default_scope lambda { order('created_at DESC') }
-  attr_accessible :body, :name, :title
+  validate :valid_time
+  attr_accessible :body, :name, :title, :created_at
+
+  def valid_time
+    if created_at.nil?
+      errors.add(:created_at, :invalid_format)
+    end
+  end
 end
---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "idcf.private")]
+default_run_options[:pty] = true
+set :application, "yabbs"
+set :repository,  "https://github.com/makimoto/yabbs"
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+#role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+role :app, "root@210.129.193.64"
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
